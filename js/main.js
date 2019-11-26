2f34590750afd8ed54af5ab4a06e750cc8a830c1
Alex <jonk6233@gmail.com>
2019-11-27 00:38:52 +0200

    change-4

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 $(function(){
-
 	$('.top__slider').slick({
 		arrows: false,
 		dots: true,
 		autoplay: true,
-		autoplaySpeed: 3000,
+		aytoplaySpedd: 3000,
 		fade: true,
-		responsive: [
-			{
-				breakpoint: 601,
-				settings: {
-					dots: false
-				}
-			}
-		]
+	});
+	$('.slick-prev').click(function(){
+		$('.top__slider').slick('slickPrev');
+	})
+	
+	$('.slick-next').click(function(){
+		$('.top__slider').slick('slickNext');
 	});
 
 	$('.single-item').slick({
@@ -24,19 +22,24 @@ $(function(){
 		fade: true,
 	});
 
-	$(".top__to-go").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+	var show = true;
+	var countbox = ".info";
+	$(window).on("scroll load resize", function () {
+			if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+			var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+			var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+			var w_height = $(window).height(); // Высота окна браузера
+			var d_height = $(document).height(); // Высота всего документа
+			var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+			if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+					$('.info-block-number').css('opacity', '1');
+					$('.info-block-number').spincrement({
+							thousandSeparator: "",
+							duration: 1200
+					});
 
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 900);
+					show = false;
+			}
 	});
-
-
 });
+
