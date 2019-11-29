a2b8b376e1f9c127a3b461fda2c5ba94f5290b94
Alex <jonk6233@gmail.com>
2019-11-29 23:39:04 +0200

    change-12

--- js/main.js
+++ js/main.js
@@ -43,3 +43,11 @@ $(function(){
 	});
 });
 
+// function for burger menu
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
